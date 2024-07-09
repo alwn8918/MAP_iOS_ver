34bccde862c91ea60c20fe264830cfb714354232
김미주 <alwn8918@gmail.com>
2024-07-10 03:23:10 +0900

    로그인 화면 틀

---
+++ MobileAppIOSVersion/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  MobileAppIOSVersion
+//
+//  Created by 김미주 on 10/07/2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    // MARK: - Properties
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var guideLabel: UILabel!
+    @IBOutlet weak var signUpLabel: UILabel!
+    @IBOutlet weak var emailBtn: UIButton!
+    @IBOutlet weak var googleBtn: UIButton!
+    @IBOutlet weak var kakaoBtn: UIButton!
+    
+
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setFont()
+
+        emailBtn.layer.cornerRadius = 5
+        googleBtn.layer.cornerRadius = 5
+        kakaoBtn.layer.cornerRadius = 5
+    }
+
+    // MARK: - Function
+    func setFont() {
+        titleLabel.font = UIFont(name: "BagelFatOne-Regular", size: 60)
+        guideLabel.font = UIFont(name: "GmarketSansTTFMedium", size: 20)
+        signUpLabel.font = UIFont(name: "GmarketSansTTFMedium", size: 13)
+    }
+}
