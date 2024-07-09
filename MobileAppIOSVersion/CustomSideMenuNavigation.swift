abb9ae886a31d5f36f3ca489b0f244dd0e0e1640
김미주 <alwn8918@gmail.com>
2024-07-10 01:53:26 +0900

    sidemenu 수정

--- MobileAppIOSVersion/CustomSideMenuNavigation.swift
+++ MobileAppIOSVersion/CustomSideMenuNavigation.swift
@@ -6,24 +6,17 @@
 //
 
 import UIKit
+import SideMenu
 
-class CustomSideMenuNavigation: UINavigationController {
+class CustomSideMenuNavigation: SideMenuNavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.presentationStyle = .menuSlideIn
+        self.presentationStyle.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        self.presentationStyle.presentingEndAlpha = 0.5
+        self.menuWidth = min(view.frame.width, view.frame.height) * 0.7
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- MobileAppIOSVersion/MainViewController.swift
+++ MobileAppIOSVersion/MainViewController.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    // MARK: - Properties
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var whereLabel: UILabel!
+    @IBOutlet weak var recentLabel: UILabel!
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setFont()
+        
+        mainView.layer.cornerRadius = 20
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Function
+    func setFont() {
+        whereLabel.font = UIFont(name: "GmarketSansTTFBold", size: 35)
+        recentLabel.font = UIFont(name: "GmarketSansTTFMedium", size: 15)
     }
-    */
-
 }
